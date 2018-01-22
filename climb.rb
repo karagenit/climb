4b691456bec6af170ab163dea49641987e294fec
Caleb Smith <karagenit@outlook.com>
2018-01-21 22:32:11 -0500

    Add Reversal Equations

--- climb.rb
+++ climb.rb
@@ -42,10 +42,15 @@ while true
 
   equations.push Belir::Equation.new(:output_torque, :mass, :radius) { |mass, radius| ((mass * GRAVITY).to_newton * radius).to_f }
   equations.push Belir::Equation.new(:motor_torque, :amps) { |amps| motor.torque(current: amps) }
+  equations.push Belir::Equation.new(:amps, :motor_torque) { |motor_torque| motor.current(torque: motor_torque) }
   equations.push Belir::Equation.new(:ratio, :motor_torque, :output_torque) { |motor_torque, output_torque| output_torque / motor_torque }
+  equations.push Belir::Equation.new(:motor_torque, :ratio, :output_torque) { |ratio, output_torque| output_torque / ratio }
   equations.push Belir::Equation.new(:motor_rpm, :amps) { |amps| motor.speed(current: amps) }
+  equations.push Belir::Equation.new(:amps, :motor_rpm) { |motor_rpm| motor.current(speed: motor_rpm) }
   equations.push Belir::Equation.new(:output_rpm, :motor_rpm, :ratio) { |motor_rpm, ratio| motor_rpm / ratio }
+  equations.push Belir::Equation.new(:motor_rpm, :output_rpm, :ratio) { |output_rpm, ratio| output_rpm * ratio }
   equations.push Belir::Equation.new(:output_speed, :output_rpm, :radius) { |output_rpm, radius| output_rpm * 2 * Math::PI * radius.to_foot.to_f / 60 } # ft/s
+  equations.push Belir::Equation.new(:output_rpm, :output_speed, :radius) { |output_speed, radius| output_speed * 60 / (2 * Math::PI * radius.to_foot.to_f) }
 
   system = Belir::System.new(*equations)
 
