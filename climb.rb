1e24a127af00b65fc8c2eec9c016c922da7114cf
Caleb Smith <karagenit@outlook.com>
2018-01-21 22:38:50 -0500

    Fix Leftover Hash Values

--- climb.rb
+++ climb.rb
@@ -11,8 +11,8 @@ weight = Unitwise(gets.to_f, 'pound')
 print "Spool Radius (in): "
 radius = Unitwise(gets.to_f, 'inch')
 
-values[:mass] = weight.to_kg
-values[:radius] = radius.to_m
+mass = weight.to_kg
+radius = radius.to_m
 GRAVITY = Unitwise(9.81, 'meter') / (Unitwise(1, 'second') ** 2)
 
 while true
@@ -38,6 +38,9 @@ while true
   # limits (give one, get the others): amps, ratio, speed
   # motor_torque, output_torque, amps, ratio, motor_rpm, output_rpm, output_speed
 
+  values = {}
+  values[:mass] = mass
+  values[:radius] = radius
   equations = []
 
   equations.push Belir::Equation.new(:output_torque, :mass, :radius) { |mass, radius| ((mass * GRAVITY).to_newton * radius).to_f }
